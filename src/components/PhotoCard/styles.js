ce32635ef3cb83e144112af3b226260f2ea9370b
Juan Felipe Arias <juantrullo@gmail.com>
2022-01-03 20:21:52 -0500

    Se crea animacion de las tarjetas

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { fadeIn } from '../../styles/animation'
 
 export const ImgWrapper = styled.div`
     border-radius: 10px;
@@ -11,12 +12,13 @@ export const ImgWrapper = styled.div`
 `
 
 export const Img = styled.img`
-    box-shadow: 0 10px 14px rgba(0, 0, 0 .2);
-    height: 100%;
-    object-fit: cover;
-    position: absolute;
-    top: 0;
-    width: 100%
+  ${fadeIn()}
+  box-shadow: 0 10px 14px rgba(0, 0, 0 0.2);
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  width: 100%;
 `
 
 export const Button = styled.button`
