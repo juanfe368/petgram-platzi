caada4ca64a567b4a0b0fa5fe5f19be5e5ff2b48
Juan Felipe Arias <juantrullo@gmail.com>
2022-01-24 21:00:56 -0500

    Nuevas mejoras en el codigo

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,25 +1,61 @@
-import React, { useEffect, useState } from 'react'
+/* eslint-disable react/jsx-fragments */
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 // import Datajson from '../../Data/db'
 
-export const ListOfCategories = () => {
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
-
+  const [loading, setLoading] = useState(false)
   useEffect(function () {
-    window.fetch('https://petgram-server-edsf8xpy2.now.sh/categories')
-      .then(res => res.json())
-      .then(response => {
+    setLoading(true)
+    window
+      .fetch('https://petgram-server-edsf8xpy2.now.sh/categories')
+      .then((res) => res.json())
+      .then((response) => {
         setCategories(response)
+        setLoading(false)
       })
   }, [])
-  return (
-    <List>
-      {categories.map((category) => (
-        <Item key={category.id}>
-          <Category {...category} />
-        </Item>
-      ))}
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
+      {
+        loading
+          ? <Item key={loading}><Category /></Item>
+          : categories.map((category) => (
+            <Item key={category.id}>
+              <Category {...category} />
+            </Item>
+          ))
+}
     </List>
+  )
+
+  if (loading) {
+    return 'Cargando...'
+  }
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 }
