bdb6d253f0bc7bc1839192b402259b196f5c6321
Juan Felipe Arias <juantrullo@gmail.com>
2022-01-26 18:56:26 -0500

    Se agregar funcionalidad para que el compoenente se cargue cuando este esta a la vista

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,24 +1,48 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
+import React, { useEffect, useRef, useState } from 'react'
+import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
+import { Fragment } from 'react/cjs/react.production.min'
 
 const DEFAULT_IMAGE =
   'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({
   id, likes = 0,
-  src
+  src = DEFAULT_IMAGE
 }) => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+  useEffect(function () {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const isIntersecting = entries[0].isIntersecting
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)
+    })
+  }, [element])
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={DEFAULT_IMAGE} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
+    <Article ref={element}>
+      {show && (
+        <Fragment>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={DEFAULT_IMAGE} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes!
+          </Button>
+        </Fragment>
+      )}
+    </Article>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
+            plugins: ['@babel/plugin-syntax-dynamic-import'],
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
